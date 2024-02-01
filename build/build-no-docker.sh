85095dbb70396b3b7c575bd3832ac4d6e5015dda
Luke Georgalas <lgeorgalas@illumina.com>
2024-02-01 11:33:08 -0800

    Removed commented line from build

--- build/build-no-docker.sh
+++ build/build-no-docker.sh
@@ -2,4 +2,4 @@
 
 dotnet sonarscanner begin /o:lukegeor /k:lukegeor_containerscan /d:sonar.token=$SONAR_TOKEN /d:sonar.host.url=https://sonarcloud.io
 make build-no-docker
-#dotnet sonarscanner end /d:sonar.token=$SONAR_TOKEN
+dotnet sonarscanner end /d:sonar.token=$SONAR_TOKEN
