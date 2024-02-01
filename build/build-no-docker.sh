5d9fc1e45b42a385574574e9bc703dde74504042
Luke Georgalas <lgeorgalas@illumina.com>
2024-02-01 09:33:03 -0800

    updated build.sh

--- build/build-no-docker.sh
+++ build/build-no-docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-SONAR_TOKEN=c8187e0b2ece7a03324d2cacd929e2ed7271b6fb
 dotnet sonarscanner begin /o:lukegeor /k:lukegeor_containerscan /d:sonar.token=$SONAR_TOKEN /d:sonar.host.url=https://sonarcloud.io
 make build-no-docker
 #dotnet sonarscanner end /d:sonar.token=$SONAR_TOKEN
